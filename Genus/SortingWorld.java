ecf454d44e8482c1064a3a4e718917f873bc0369
kshah301993 <kshah301993@gmail.com>
2016-10-25 22:52:57 -0700

    Adding sounds

--- Genus/SortingWorld.java
+++ Genus/SortingWorld.java
@@ -26,6 +26,7 @@ public class SortingWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 600, 1); 
+        Greenfoot.playSound("game_sound.mp3");
         screen.showScreen(); 
         // This goes in welcome screen
 
