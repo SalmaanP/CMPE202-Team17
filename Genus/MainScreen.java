b3c15ce9f31668e2048e0fbf1640c2f37be65a7d
Salmaan Pehlari <salmaanpehlari@gmail.com>
2016-11-08 22:37:45 -0800

    cleaned code

--- Genus/MainScreen.java
+++ Genus/MainScreen.java
@@ -60,45 +60,6 @@ public class MainScreen  extends Screen
       }
     
       
-       //put for loop here
-       // Ball ball1 = new Ball(1);
-       // ball1.setImage("yarn.png");
-       // ball1.setPos(1);
-       // Ball ball2 = new Ball(2);
-       // ball2.setImage("frog.png");
-       // ball2.setPos(2);
-       // Ball ball3 = new Ball(3);
-       // ball3.setImage("cart.png");
-       // ball3.setPos(3);
-       // Ball ball4 = new Ball(4);
-       // ball4.setImage("bag.png");
-       // ball4.setPos(4);
-       // Ball ball5 = new Ball(5);
-       // ball5.setImage("robo.png");
-       // ball5.setPos(5);
-       // Ball ball6 = new Ball(6);
-       // ball6.setImage("light.png");
-       // ball6.setPos(6);
-       
-       // Ball [] balls = {ball1,ball2,ball3,ball4,ball5,ball6};
-       // List<Ball> ball_list = new ArrayList<Ball>(Arrays.asList(balls));
-       
-       
-
-        // for(int i = 0; (i<5) && (ball_list.size() > 0); i ++) {
-            // final int randomIndex = rand.nextInt(ball_list.size());
-            // Ball temp = ball_list.remove(randomIndex);
-            // System.out.println(temp.getPos());
-        // }
-       
-       // //implement for loop here
-       // world.addObject(weighingMachine, 500, 275);
-       // world.addObject(ball1,ball1.getX() ,500 );
-       // world.addObject(ball2,ball2.getX() ,500 );
-       // world.addObject(ball3,ball3.getX() ,500 );
-       // world.addObject(ball4,ball4.getX() ,500 );
-       // world.addObject(ball5,ball5.getX() ,500 );
-       // world.addObject(ball6,ball6.getX() ,500 );
         timer= new Timer();
         world.addObject(timer,875 ,85 );
        
