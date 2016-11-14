854f68a7292e1beff2d17c2f18e5abc30f9b4c73
maitray <maitray.shah@sjsu.edu>
2016-11-14 09:54:40 -0800

    Leaderboard and Time update

--- Genus/Text.java
+++ Genus/Text.java
@@ -9,22 +9,25 @@ import java.awt.Color;
 */
 public class Text extends Actor
 {
-  GreenfootImage image;
+    GreenfootImage image , textImage;
     public void act() 
-    {
-       
-    }
+    {}
     
     Text()
     {
-        image= this.getImage();
-        setImage(image);
+      image = this.getImage();
+      setImage(image);
     }
     
     public void setMessage(String message)
     {
-        image.setColor( java.awt.Color.BLACK );
-        image.drawString(message,25, 50);
+        textImage = new GreenfootImage(message, 20, Color.WHITE, new Color(0,0,0,0));
+        image = new GreenfootImage(textImage.getWidth()+20, textImage.getHeight()+10);
+        image.drawRect(0, 0, image.getWidth(), image.getHeight());
+        image.drawImage(textImage, (image.getWidth()-textImage.getWidth())/2, (image.getHeight()-textImage.getHeight())/2);
+        setImage(image);
+       // image.setColor(java.awt.Color.BLACK);
+       // image.drawString(message, 25, 50);
     }
    
 }
